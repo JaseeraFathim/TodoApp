30232148f602ad1eba1168e3eb37010e5b5cc981
JaseeraFathim <jaseerafathim1@gmail.com>
2022-05-25 19:26:07 +0530

    first commit

---
+++ todoapp/models.py
@@ -0,0 +1,18 @@
+from curses.ascii import FS
+from datetime import date
+from importlib.resources import contents
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    content = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False)
+    
+class Meta:
+    ordering = ['-date'] 
+         
+         
+    def __str__(self):
+        return self.content
