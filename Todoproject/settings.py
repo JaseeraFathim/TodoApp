90fa1b46147b46ac0e20c4393ff5e61559890eba
JaseeraFathim <85502815+JaseeraFathim@users.noreply.github.com>
2022-05-26 23:12:46 +0530

    Update settings.py

--- Todoproject/settings.py
+++ Todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ug!^xt!_&h6#(0z=f#nn-x8-dg+7ltje!^qjp-f25$t=$_ta5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://todoappproject.vercel.app/']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
